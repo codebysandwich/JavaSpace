5d3e457b6e7c5063a7bbbe91952b3a0f48125452
sandwich <122079260@qq.com>
2022-08-12 14:09:51 +0800

    update test

---
+++ HSP_Code/Test.java
@@ -0,0 +1,17 @@
+/**
+ * File              : Test.java
+ * Author            : sandwich <122079260@qq.com>
+ * Date              : 2022-08-01 15:38:24
+ * Last Modified Date: 2022-08-06 16:57:16
+ * Last Modified By  : sandwich <122079260@qq.com>
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("3 + 2 = " + add(3, 2));
+	}
+
+	public static int add(int x, int y) {
+		return x + y;
+	}
+}
