944a9bdc7b45063755b525a1bcd26c81332fd408
sandwich <122079260@qq.com>
2022-08-14 23:27:35 +0800

    update Types course

--- Mosh/README.md
+++ Mosh/README.md
@@ -1,7 +1,8 @@
 # 跟 Mosh 学习 java
 
 ![封面](https://i2.hdslb.com/bfs/archive/aee90370841024ad4c36eb0551ee97af709a5817.jpg)
-学习视频地址:[bilibili]()
+
+学习视频地址:[bilibili](https://b23.tv/1ZkWVak)
 
 ## Foundations
 
@@ -9,3 +10,16 @@
 ## Types
 
 ```变量```计算机中临时存储数据的地方
+
+- 八种基本数据类型
+
+|Type|Bytes|Range|
+|:---|:---|:---|
+|byte|1|[-128, 127]|
+|short|2|[-32k, 32k]|
+|int|4||
+|long|8||
+|float|4||
+|double|8||
+|char|2||
+|boolean|1|true/false|
--- Mosh/src/com/mosh/Types/Main.java
+++ Mosh/src/com/mosh/Types/Main.java
@@ -1,6 +1,9 @@
 package com.mosh.Types;
 
 
+import java.awt.*;
+import java.util.Date;
+
 /**
  * @Author: Sandwich
  * @Description: Mosh讲解数据类型变量等内容的代码示例
@@ -11,11 +14,57 @@ package com.mosh.Types;
 public class Main {
     /**
      * 程序入口
+     *
      * @param args 命令行参数
      */
     public static void main(String[] args) {
-        int myAge = 30;
+        byte myAge = 30;
         int herAge = myAge + 3;
-        System.out.println(herAge);
+        // 辅助长数值阅读
+        int viewCounts = 123_456_789;
+        // 数值默认为int，long必须显示定义
+        long maxViewCounts = 3_123_456_789L;
+        // 同样小数会被默认为double类型,声明时需要显示制定float类型
+        float price = 10.99F;
+        char chr = 'A';
+        // format 输出
+        System.out.printf("age: %d; viewCounts: %,d\n", herAge, viewCounts);
+        System.out.printf("maxCount: %,d; price is %.2f\n", maxViewCounts, price);
+        // chr 本质上还是数值型，支持加减法
+        System.out.printf("char %c\n", chr + 1);
+        //-------------------------------------------------------------//
+        ShowPrimitiveAndReference();
+        ShowStringDetail();
     }
+
+    /**
+     * 演示值类型与引用类型的区别
+     */
+    public static void ShowPrimitiveAndReference() {
+        byte x, y = 1;
+        x = 2;
+        System.out.printf("x: %d, y: %d\n", x, y);
+        Point point1 = new Point(1, 1);
+        Point point2;
+        point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+
+        Date now = new Date();
+        System.out.println(now.getTime());
+    }
+
+    /**
+     * 演示字符串相关细节
+     */
+    public static void ShowStringDetail() {
+        String message = "hello world" + " !!";
+        System.out.println(message.endsWith("!!"));
+        // 字符串不可修改的特性
+        System.out.println(message.replace('!', '*'));
+        System.out.println(message);
+        System.out.println(message.toUpperCase());
+        System.out.println(message);
+    }
+
 }
